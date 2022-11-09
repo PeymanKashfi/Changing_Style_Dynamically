2cd211cdcb9a0f4940083a672c4e9f26e0109884
Peyman Kashfi <p.kashfi@gmail.com>
2022-11-10 09:13:43 +1100

    Chaning body background by clicking a button using onclick event

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//document.querySelector(".btn-colour").addEventListener("click", () => {
+
+//});
+
+const changeBColour = () => {
+  /*let x = document.createElement("label");
+  let t = document.createTextNode("Test!");
+  x.appendChild(t);
+  document.body.appendChild(x);*/
+  document.body.style.backgroundColor = "red";
+};
