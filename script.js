1c893d4c125fecdc7810d982c4c9dfea56e5b0b2
Peyman Kashfi <p.kashfi@gmail.com>
2022-11-10 11:48:04 +1100

    Event listener added

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 "use strict";
 
-//document.querySelector(".btn-colour").addEventListener("click", () => {
+document.querySelector(".btn-colour").addEventListener("click", () => {
+  document.querySelector("body").style.backgroundColor = "#0000ff";
+});
 
-//});
-
-const changeBColour = () => {
+document.querySelector(".btn-colour").addEventListener("click", () => {
+  let x = document.createElement("label");
+  let t = document.createTextNode("Test!");
+  x.appendChild(t);
+  document.body.appendChild(x);
+  document.querySelector("body").appendChild();
+});
+/*const changeBColour = () => {
   /*let x = document.createElement("label");
   let t = document.createTextNode("Test!");
   x.appendChild(t);
-  document.body.appendChild(x);*/
+  document.body.appendChild(x);
   document.querySelector("body").style.backgroundColor = "#ff0A22";
   //document.body.style.backgroundColor = "red";
-};
+};*/
